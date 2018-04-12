3e2e6c04fa5a11b4c8c674ad639e7f5eb6799cb0
VALENTIN\valik <valik.sparov.ru1@gmail.com>
2018-04-12 23:44:27 +0300

    no message

---
+++ Assets/Supercyan Character Pack Free Sample/Animations/Open.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour {
+public AnimationClip animationOpen;
+	public GameObject OD;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown ()
+    {
+        OD.gameObject.PlayAnimation(GameObjekt animationOpen.name);
+    }
+}
