393df1263d3ca5b10600d8c26df0503430d197bd
VALENTIN\valik <valik.sparov.ru1@gmail.com>
2018-04-14 19:27:04 +0300

    Анимация двери

---
+++ Assets/Animation/Wardboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wardboard : MonoBehaviour {
+	public Animator animController;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*if (Input.GetKeyDown(KeyCode.R))
+		{
+			animController.SetBool("IdleBool", true);
+		}*/
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			animController.SetBool("IdleBool", false);
+		}
+		
+	}
+}
