c261991b022abc6216ec7225b878c1d95a7a66bb
TheWhiteUnicorn <thewhiteunicorn99@gmail.com>
2018-05-21 12:37:41 +0300

    Изменения в конфигах

--- Assets/Scripts/ParamsConfig.cs
+++ Assets/Scripts/ParamsConfig.cs
@@ -4,14 +4,14 @@ namespace Stats {
 	public struct Params {
 		// Крайние значения статов
 		public const int MIN_ZERO = 0;
-		public const int MAX_HUNGER = 100;
+		public const int MAX_FOOD = 100;
 		public const int MAX_ENERGY = 100;
 		public const float MIN_MARKS = 2;
 		public const float MAX_MARKS = 5;		
 		public const int MAX_POPULARITY_LEVEL = 10;
 
 		// Начальные значения статов
-		public const int INIT_HUNGER = 100;
+		public const int INIT_FOOD = 100;
 		public const int INIT_ENERGY = 100;
 		public const float INIT_MARKS = 4;
 		public const int INIT_POPULARITY_POINTS = 0;
@@ -26,15 +26,15 @@ namespace Stats {
         // Эффекты от активностей
         // *Голод/энергия
         public const int STUDY_ENERGY_COST = 40;
-        public const int STUDY_HUNGER_COST = 15;
+        public const int STUDY_FOOD_COST = 15;
         public const int LABS_ENERGY_COST = 30;
-        public const int LABS_HUNGER_COST = 15;
+        public const int LABS_FOOD_COST = 15;
         public const int SK_ENERGY_COST = 50;
-        public const int SK_HUNGER_COST = 50;
+        public const int SK_FOOD_COST = 50;
         public const int JYM_ENERGY_COST = 50;
-        public const int JYM_HUNGER_COST = 50;
+        public const int JYM_FOOD_COST = 50;
         public const int JOB_ENERGY_COST = 50;
-        public const int JOB_HUNGER_COST = 50;
+        public const int JOB_FOOD_COST = 50;
 
         // *Влияние на успевамость
         public const float MARKS_INCREASE_STUDY = 1.25f;
